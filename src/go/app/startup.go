sandialabs/sceptre-phenix

commit 7faa5db51cb8bf5e7e9a016529be08f57c916ba1

Remove unneeded print statements added in 2c92700

src/go/app/startup.go
@@ -154,8 +154,6 @@ func (Startup) PostStart(ctx context.Context, exp *types.Experiment) error {
 		if strings.EqualFold(node.Hardware().OSType(), "windows") {
 			// Windows 10 doesn't automatically run scripts in the startup folder
 			if ver, ok := node.GetAnnotation("windows-version"); ok && ver == "10" {
-				fmt.Printf("creating C2 command to run Windows startup script for %s\n", node.General().Hostname())
-
 				id, err := mm.ExecC2Command(
 					mm.C2NS(exp.Metadata.Name),
 					mm.C2VM(node.General().Hostname()),
@@ -166,8 +164,6 @@ func (Startup) PostStart(ctx context.Context, exp *types.Experiment) error {
 				if err != nil {
 					return fmt.Errorf("execute C2 command to run Windows startup script: %w", err)
 				}
-
-				fmt.Printf("C2 command ID: %s\n", id)
 			}
 		}
 	}

commit ee563423bfc4694902cf70de2f62143e78824e65

Fix unused variable error introduced in 7faa5db

src/go/Makefile
@@ -106,3 +106,7 @@ web/proto/vm.pb.go: web/proto/*.proto bin/protoc-gen-go
 bin/phenix: $(GOSOURCES) generate-bindata generate-protobuf
 	mkdir -p bin
 	CGO_ENABLED=0 GOOS=linux go build -a -ldflags="-X 'phenix/version.Version=$(VERSION)' -s -w" -trimpath -o bin/phenix main.go
+
+bin/phenix-debug: $(GOSOURCES) generate-bindata generate-protobuf
+	mkdir -p bin
+	CGO_ENABLED=0 GOOS=linux go build -gcflags "all=-N -l" -trimpath -o bin/phenix-debug main.go

src/go/app/startup.go
@@ -154,7 +154,7 @@ func (Startup) PostStart(ctx context.Context, exp *types.Experiment) error {
 		if strings.EqualFold(node.Hardware().OSType(), "windows") {
 			// Windows 10 doesn't automatically run scripts in the startup folder
 			if ver, ok := node.GetAnnotation("windows-version"); ok && ver == "10" {
-				id, err := mm.ExecC2Command(
+				_, err := mm.ExecC2Command(
 					mm.C2NS(exp.Metadata.Name),
 					mm.C2VM(node.General().Hostname()),
 					mm.C2SkipActiveClientCheck(true),