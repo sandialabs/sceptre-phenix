sandialabs/sceptre-phenix

commit 0b680049c6c36240e69ba67924d3086df99e25e7

Add build arg for setting non-default base path for client-side UI

src/js/src/main.js
@@ -80,7 +80,9 @@ Vue.filter( 'uptime', function( value ) {
   }
 })
 
-Vue.http.options.root = '/api/v1/'
+// This needs to be relative path in order to work correctly when used on a URL
+// with a base path other than "/".
+Vue.http.options.root = 'api/v1/'
 
 Vue.http.interceptors.push(
   request => {

docker-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-usage="usage: $(basename "$0") [-d] [-h] [-v]
+usage="usage: $(basename "$0") [-b] [-d] [-h] [-v]
 
 This script will build the phenix binary using a temporary Docker image to
 avoid having to install build dependencies locally.
@@ -13,19 +13,22 @@ If not provided, the '-v' flag will default to the hash of the current git
 repository commit.
 
 where:
+    -b      base path for web UI deployment (defaults to '/')
     -d      disable phenix web UI authentication
     -h      show this help text
     -v      version number to use for phenix"
 
 
+base=/
 auth=enabled
 version=$(git log -1 --format="%h")
 commit=$(git log -1 --format="%h")
 
 
 # loop through positional options/arguments
-while getopts ':dhv:' option; do
+while getopts ':b:dhv:' option; do
     case "$option" in
+        b)  base="$OPTARG"         ;;
         d)  auth=disabled          ;;
         h)  echo -e "$usage"; exit ;;
         v)  version="$OPTARG"      ;;
@@ -36,6 +39,7 @@ while getopts ':dhv:' option; do
 done
 
 
+echo    "phenix web UI base path:      $base"
 echo    "phenix web UI authentication: $auth"
 echo    "phenix version number:        $version"
 echo -e "phenix commit:                $commit\n"
@@ -99,6 +103,7 @@ docker run -it --rm \
   -w /phenix \
   -u $USERNAME \
   -e VUE_APP_AUTH=$auth \
+  -e VUE_BASE_PATH=$base \
   -e VER=$version \
   -e COMMIT=$commit \
   phenix:builder make bin/phenix

docker/Dockerfile
@@ -7,7 +7,10 @@ COPY ./src/js /phenix/src/js
 WORKDIR /phenix/src/js
 
 ARG PHENIX_WEB_AUTH=disabled
+ARG PHENIX_BASE_PATH=/
+
 ENV VUE_APP_AUTH ${PHENIX_WEB_AUTH}
+ENV VUE_BASE_PATH ${PHENIX_BASE_PATH}
 
 RUN npm install \
   && npm run build

src/js/vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  publicPath: process.env.VUE_BASE_PATH || '/',
   assetsDir: 'assets',
 
   devServer: {

commit 788f34b9c72f048c0084be743202c9fa043db22e

Prepend base path to root URL in UI client
Fixes bug introduced in commit 0b68004.

src/js/src/main.js
@@ -80,9 +80,11 @@ Vue.filter( 'uptime', function( value ) {
   }
 })
 
-// This needs to be relative path in order to work correctly when used on a URL
-// with a base path other than "/".
-Vue.http.options.root = 'api/v1/'
+if ( process.env.BASE_URL === '/' ) {
+  Vue.http.options.root = '/api/v1/'
+} else {
+  Vue.http.options.root = process.env.BASE_URL + '/api/v1/'
+}
 
 Vue.http.interceptors.push(
   request => {