sandialabs/sceptre-phenix

commit dd040e5ec8906b68c6c3b57f5929681019fa7780

Fix bug scanning results from user apps over stdout

src/go/app/user.go
@@ -100,6 +100,7 @@ func (this UserApp) shellOut(ctx context.Context, action Action, exp *types.Expe
 		shell.Command(cmdName),
 		shell.Args(string(action)),
 		shell.Stdin(data),
+		shell.SplitWords(),
 		shell.Env(
 			"PHENIX_DIR="+common.PhenixBase,
 			"PHENIX_LOG_LEVEL="+util.GetEnv("PHENIX_LOG_LEVEL", "DEBUG"),

src/go/util/shell/option.go
@@ -1,6 +1,9 @@
 package shell
 
-import "os"
+import (
+	"bufio"
+	"os"
+)
 
 type Option func(*options)
 
@@ -12,11 +15,14 @@ type options struct {
 
 	stdout chan []byte
 	stderr chan []byte
+
+	splitter bufio.SplitFunc
 }
 
 func newOptions(opts ...Option) options {
 	o := options{
-		env: os.Environ(),
+		env:      os.Environ(),
+		splitter: bufio.ScanLines,
 	}
 
 	for _, opt := range opts {
@@ -59,5 +65,17 @@ func StreamStdout(s chan []byte) Option {
 func StreamStderr(s chan []byte) Option {
 	return func(o *options) {
 		o.stderr = s
+	}
+}
+
+func SplitLines() Option {
+	return func(o *options) {
+		o.splitter = bufio.ScanLines
+	}
+}
+
+func SplitWords() Option {
+	return func(o *options) {
+		o.splitter = bufio.ScanWords
 	}
 }

src/go/util/shell/exec.go
@@ -127,6 +127,8 @@ func (shell) ExecCommand(ctx context.Context, opts ...Option) ([]byte, []byte, e
 
 	go func() {
 		scanner := bufio.NewScanner(stdout)
+		scanner.Split(o.splitter)
+
 		for scanner.Scan() {
 			bytes := scanner.Bytes()
 
@@ -144,6 +146,8 @@ func (shell) ExecCommand(ctx context.Context, opts ...Option) ([]byte, []byte, e
 
 	go func() {
 		scanner := bufio.NewScanner(stderr)
+		scanner.Split(bufio.ScanLines)
+
 		for scanner.Scan() {
 			bytes := scanner.Bytes()
 

commit 5e4743d75f85844cab81d8653bd5904053a8242f

Fix bug introduced by dd040e5 that removed legitimate spaces in strings

src/go/app/user.go
@@ -100,7 +100,7 @@ func (this UserApp) shellOut(ctx context.Context, action Action, exp *types.Expe
 		shell.Command(cmdName),
 		shell.Args(string(action)),
 		shell.Stdin(data),
-		shell.SplitWords(),
+		shell.SplitBytes(),
 		shell.Env(
 			"PHENIX_DIR="+common.PhenixBase,
 			"PHENIX_LOG_LEVEL="+util.GetEnv("PHENIX_LOG_LEVEL", "DEBUG"),

src/go/util/shell/option.go
@@ -74,8 +74,8 @@ func SplitLines() Option {
 	}
 }
 
-func SplitWords() Option {
+func SplitBytes() Option {
 	return func(o *options) {
-		o.splitter = bufio.ScanWords
+		o.splitter = bufio.ScanBytes
 	}
 }